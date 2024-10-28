da5d84744a6a1a3b5729f2326fe35b04de354f61
Yanuo Ma <myn508@hotmail.com>
2024-10-28 18:21:44 +0000

    Add other missing highligh groups from lush template

--- lua/lush_theme/github_light.lua
+++ lua/lush_theme/github_light.lua
@@ -21,38 +21,93 @@ local theme = lush(function(injected_functions)
   local sym = injected_functions.sym
 
   return {
-    Normal { fg = palette.gray[9], bg = palette.white },                     -- Normal text
-    Variable { fg = palette.black },                                         -- Variables and identifiers
-    Comment { fg = palette.gray[6] },                                        -- Comments
-    Identifier { fg = palette.blue[7] },                                     -- Variables and identifiers
-    Function { fg = palette.purple[6] },                                     -- Functions and methods
-    Statement { fg = palette.red[6], },                                      -- Statements like `if`, `for`, etc.
-    Keyword { fg = palette.red[6] },                                         -- Keywords like `return`, `function`
-    Operator { fg = palette.red[6] },                                        -- Operators like `+`, `-`, `*`, etc.
-    Constant { fg = palette.blue[7] },                                       -- Constants like numbers, booleans
-    String { fg = palette.blue[9] },                                         -- Strings
-    Type { fg = palette.red[6] },                                            -- Types and classes
-    PreProc { fg = palette.red[6] },                                         -- Preprocessor commands
-    Special { fg = palette.purple[6] },                                      -- Special symbols
-    Visual { bg = palette.blue[1] },                                         -- Visual selection
-    CursorLine { bg = palette.gray[2] },                                     -- Cursor line
-    LineNr { fg = palette.gray[4] },                                         -- Line numbers
-    CursorLineNr { fg = palette.black },                                     -- Cursor line number
-    MatchParen { fg = palette.red[6], gui = 'bold,underline' },              -- Matching parenthesis
-    Pmenu { fg = palette.gray[8], bg = palette.gray[1] },                    -- Popup menu
-    PmenuSel { bg = palette.blue[2] },                                       -- Popup menu selection
-    VertSplit { fg = palette.gray[3] },                                      -- Vertical split line
-    StatusLine { fg = palette.gray[8], bg = palette.gray[1] },               -- Status line
-    StatusLineNC { fg = palette.gray[5], bg = palette.gray[1] },             -- Non-current status line
-    Removed { fg = palette.red[6] },                                         -- Removed text
-    Changed { fg = palette.blue[6] },                                        -- Changed text
-    Added { fg = palette.green[6] },                                         -- Added text
-    NonText { fg = palette.gray[3], bg = palette.white },                    -- Non-text characters
+    Normal { fg = palette.gray[9], bg = palette.white },                       -- Normal text
+    Variable { fg = palette.black },                                           -- Variables and identifiers
+    Comment { fg = palette.gray[6] },                                          -- Comments
+    Identifier { fg = palette.blue[7] },                                       -- Variables and identifiers
+    Function { fg = palette.purple[6] },                                       -- Functions and methods
+    Statement { fg = palette.red[6], },                                        -- Statements like `if`, `for`, etc.
+    Keyword { fg = palette.red[6] },                                           -- Keywords like `return`, `function`
+    Operator { fg = palette.red[6] },                                          -- Operators like `+`, `-`, `*`, etc.
+    Constant { fg = palette.blue[7] },                                         -- Constants like numbers, booleans
+    String { fg = palette.blue[9] },                                           -- Strings
+    Type { fg = palette.red[6] },                                              -- Types and classes
+    PreProc { fg = palette.red[6] },                                           -- Preprocessor commands
+    Special { fg = palette.purple[6] },                                        -- Special symbols
+    Visual { bg = palette.blue[1] },                                           -- Visual selection
+    CursorLine { bg = palette.gray[2] },                                       -- Cursor line
+    LineNr { fg = palette.gray[4] },                                           -- Line numbers
+    CursorLineNr { fg = palette.black },                                       -- Cursor line number
+    MatchParen { fg = palette.red[6], gui = 'bold,underline' },                -- Matching parenthesis
+    Pmenu { fg = palette.gray[8], bg = palette.gray[1] },                      -- Popup menu
+    PmenuSel { bg = palette.blue[2] },                                         -- Popup menu selection
+    VertSplit { fg = palette.gray[3] },                                        -- Vertical split line
+    StatusLine { fg = palette.gray[8], bg = palette.gray[1] },                 -- Status line
+    StatusLineNC { fg = palette.gray[5], bg = palette.gray[1] },               -- Non-current status line
+    Removed { fg = palette.red[6] },                                           -- Removed text
+    Changed { fg = palette.blue[6] },                                          -- Changed text
+    Added { fg = palette.green[6] },                                           -- Added text
+    NonText { fg = palette.gray[3], bg = palette.white },                      -- Non-text characters
 
-    DiffAdd { fg = palette.green[6], bg = palette.green[1] },                -- Diff mode: Added line
-    DiffChange { fg = palette.green[6], bg = palette.green[1] },             -- Diff mode: Changed line
-    DiffDelete { fg = palette.red[7], bg = palette.red[2] },                 -- Diff mode: Deleted line
-    DiffText { fg = palette.green[6], bg = palette.green[3], gui = 'bold' }, -- Diff mode: Changed text within a changed line
+    ColorColumn { bg = palette.gray[2] },                                      -- Columns set with 'colorcolumn'
+    Conceal { fg = palette.gray[4] },                                          -- Placeholder characters for concealed text
+    Cursor { fg = palette.black, bg = palette.blue[6] },                       -- Character under the cursor
+    CurSearch { fg = palette.white, bg = palette.yellow[5], gui = 'bold' },    -- Search pattern under the cursor
+    lCursor { fg = palette.black, bg = palette.blue[6] },                      -- Character under the cursor with language mapping
+    CursorIM { fg = palette.black, bg = palette.blue[6] },                     -- Like Cursor, but in IME mode
+    CursorColumn { bg = palette.gray[1] },                                     -- Screen-column at the cursor
+    Directory { fg = palette.blue[6] },                                        -- Directory names in listings
+    EndOfBuffer { fg = palette.gray[4] },                                      -- Filler lines after the end of buffer
+    TermCursor { fg = palette.black, bg = palette.blue[6] },                   -- Cursor in a focused terminal
+    TermCursorNC { fg = palette.black, bg = palette.gray[5] },                 -- Cursor in an unfocused terminal
+    ErrorMsg { fg = palette.red[6], gui = 'bold' },                            -- Error messages on the command line
+    Folded { fg = palette.gray[6], bg = palette.gray[1] },                     -- Line used for closed folds
+    FoldColumn { fg = palette.gray[4], bg = palette.gray[1] },                 -- 'foldcolumn'
+    SignColumn { fg = palette.gray[5], bg = palette.white },                   -- Column where signs are displayed
+    IncSearch { fg = palette.yellow[7], bg = palette.gray[3], gui = 'bold' },  -- 'incsearch' highlighting
+    Substitute { fg = palette.white, bg = palette.orange[5], gui = 'bold' },   -- Replacement text highlighting
+    LineNrAbove { fg = palette.gray[4] },                                      -- Line number above the cursor line
+    LineNrBelow { fg = palette.gray[4] },                                      -- Line number below the cursor line
+    CursorLineFold { fg = palette.gray[6], bg = palette.gray[1] },             -- FoldColumn when 'cursorline' is set
+    CursorLineSign { fg = palette.gray[5], bg = palette.gray[1] },             -- SignColumn when 'cursorline' is set
+    ModeMsg { fg = palette.blue[6], gui = 'bold' },                            -- 'showmode' message
+    MsgArea { fg = palette.gray[9], bg = palette.white },                      -- Area for messages and cmdline
+    MsgSeparator { fg = palette.gray[4] },                                     -- Separator for scrolled messages
+    MoreMsg { fg = palette.green[6], gui = 'bold' },                           -- More-prompt
+    NormalFloat { fg = palette.gray[9], bg = palette.gray[1] },                -- Normal text in floating windows
+    FloatBorder { fg = palette.gray[5], bg = palette.gray[1] },                -- Border of floating windows
+    FloatTitle { fg = palette.blue[6], gui = 'bold' },                         -- Title of floating windows
+    NormalNC { fg = palette.gray[8], bg = palette.white },                     -- Normal text in non-current windows
+    PmenuKind { fg = palette.purple[6] },                                      -- Popup menu: Normal item "kind"
+    PmenuKindSel { fg = palette.white, bg = palette.purple[6] },               -- Popup menu: Selected item "kind"
+    PmenuExtra { fg = palette.orange[6] },                                     -- Popup menu: Normal item "extra text"
+    PmenuExtraSel { fg = palette.white, bg = palette.orange[6] },              -- Popup menu: Selected item "extra text"
+    PmenuSbar { bg = palette.gray[2] },                                        -- Popup menu: Scrollbar
+    PmenuThumb { bg = palette.gray[4] },                                       -- Popup menu: Thumb of the scrollbar
+    Question { fg = palette.green[6] },                                        -- Hit-enter prompt and yes/no questions
+    QuickFixLine { fg = palette.blue[6], bg = palette.gray[1], gui = 'bold' }, -- Current quickfix item
+    Search { fg = palette.white, bg = palette.yellow[5], gui = 'bold' },       -- Last search pattern highlighting
+    SpecialKey { fg = palette.orange[5] },                                     -- Unprintable characters
+    SpellBad { sp = palette.red[6], gui = 'undercurl' },                       -- Word not recognized by spellchecker
+    SpellCap { sp = palette.blue[6], gui = 'undercurl' },                      -- Word that should start with a capital
+    SpellLocal { sp = palette.green[6], gui = 'undercurl' },                   -- Word recognized as local to another region
+    SpellRare { sp = palette.purple[6], gui = 'undercurl' },                   -- Rarely used word
+    TabLine { fg = palette.gray[6], bg = palette.gray[2] },                    -- Tab pages line, not active tab
+    TabLineFill { bg = palette.gray[2] },                                      -- Tab pages line, where there are no labels
+    TabLineSel { fg = palette.blue[6], bg = palette.gray[1], gui = 'bold' },   -- Active tab page label
+    Title { fg = palette.gray[6], gui = 'bold' },                             -- Titles for output
+    VisualNOS { bg = palette.red[1] },                                         -- Visual mode selection when not owning selection
+    WarningMsg { fg = palette.yellow[6], gui = 'bold' },                       -- Warning messages
+    Whitespace { fg = palette.gray[3] },                                       -- Whitespace characters
+    Winseparator { fg = palette.gray[3] },                                     -- Separator between window splits
+    WildMenu { fg = palette.white, bg = palette.blue[6], gui = 'bold' },       -- Current match in 'wildmenu' completion
+    WinBar { fg = palette.blue[6], bg = palette.gray[2], gui = 'bold' },       -- Window bar of current window
+    WinBarNC { fg = palette.gray[6], bg = palette.gray[2] },                   -- Window bar of not-current windows
+
+    DiffAdd { fg = palette.green[6], bg = palette.green[1] },                  -- Diff mode: Added line
+    DiffChange { fg = palette.green[6], bg = palette.green[1] },               -- Diff mode: Changed line
+    DiffDelete { fg = palette.red[7], bg = palette.red[2] },                   -- Diff mode: Deleted line
+    DiffText { fg = palette.green[6], bg = palette.green[3], gui = 'bold' },   -- Diff mode: Changed text within a changed line
 
     DiagnosticError { fg = palette.red[7] },
     DiagnosticWarn { fg = palette.yellow[7] },
