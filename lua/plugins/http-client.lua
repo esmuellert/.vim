158fafa198b07df25f74699a01a8733274599e6d
Yanuo Ma <myn508@hotmail.com>
2025-10-22 17:25:14 -0400

    kulala key bind on load

--- lua/plugins/http-client.lua
+++ lua/plugins/http-client.lua
@@ -10,17 +10,6 @@ return {
     "mistweaverco/kulala.nvim",
     enabled = enabled.kulala,
     ft = { "http", "rest" },
-    keys = {
-      { "<leader>Rs", "<cmd>lua require('kulala').run()<cr>", desc = "Send the request" },
-      { "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle headers/body" },
-      { "<leader>Rp", "<cmd>lua require('kulala').jump_prev()<cr>", desc = "Jump to previous request" },
-      { "<leader>Rn", "<cmd>lua require('kulala').jump_next()<cr>", desc = "Jump to next request" },
-      { "<leader>Ri", "<cmd>lua require('kulala').inspect()<cr>", desc = "Inspect current request" },
-      { "<leader>Re", "<cmd>lua require('kulala').set_selected_env()<cr>", desc = "Set environment" },
-      { "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy as cURL" },
-      { "<leader>RC", "<cmd>lua require('kulala').from_curl()<cr>", desc = "Paste from cURL" },
-      { "<leader>Rb", "<cmd>lua require('kulala').scratchpad()<cr>", desc = "Open scratchpad" },
-    },
     opts = {
       default_view = "body",
       default_env = "dev",
@@ -43,5 +32,19 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("kulala").setup(opts)
+      
+      -- Set up keymaps (only loaded when plugin loads)
+      vim.keymap.set("n", "<leader>Rs", "<cmd>lua require('kulala').run()<cr>", { desc = "Send the request" })
+      vim.keymap.set("n", "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", { desc = "Toggle headers/body" })
+      vim.keymap.set("n", "<leader>Rp", "<cmd>lua require('kulala').jump_prev()<cr>", { desc = "Jump to previous request" })
+      vim.keymap.set("n", "<leader>Rn", "<cmd>lua require('kulala').jump_next()<cr>", { desc = "Jump to next request" })
+      vim.keymap.set("n", "<leader>Ri", "<cmd>lua require('kulala').inspect()<cr>", { desc = "Inspect current request" })
+      vim.keymap.set("n", "<leader>Re", "<cmd>lua require('kulala').set_selected_env()<cr>", { desc = "Set environment" })
+      vim.keymap.set("n", "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", { desc = "Copy as cURL" })
+      vim.keymap.set("n", "<leader>RC", "<cmd>lua require('kulala').from_curl()<cr>", { desc = "Paste from cURL" })
+      vim.keymap.set("n", "<leader>Rb", "<cmd>lua require('kulala').scratchpad()<cr>", { desc = "Open scratchpad" })
+    end,
   },
 }
