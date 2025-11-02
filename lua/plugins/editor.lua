3601df897fa12d84dfb6c26c12d08cddcb05b9a9
Yanuo Ma <myn508@hotmail.com>
2025-11-02 01:32:54 -0400

    Plugin rename

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -56,7 +56,7 @@ return {
     enabled = enabled.vscode_diff,
     pin = false,
     build = vim.fn.has('win32') == 1 and 'cmd.exe /c build.cmd' or 'bash build.sh',
-    cmd = 'VscodeDiff',
+    cmd = 'VSCodeDiff',
     config = function()
       require('vscode-diff.config').setup({})
     end,
