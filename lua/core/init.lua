357a6b4f455ee837edf933ec33ff236e3ed628ca
Yanuo Ma <myn508@hotmail.com>
2025-11-03 17:02:37 -0500

    Fix file_reload.lua broken

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -4,13 +4,8 @@ require("core.options")
 require("core.keymaps")
 require("core.autocmds")
 
--- Setup intelligent file reload handling
-require("core.file_reload").setup({
-  -- notify_on_reload = true,  -- Show notification when file reloads
-  -- notify_timeout = 3000,     -- Notification duration in ms
-  -- auto_reload_if_unchanged = true,  -- Auto-reload if no unsaved changes
-  -- backup_if_changed = true,  -- Backup unsaved changes before external reload
-})
+-- Setup intelligent file reload handling (auto-setup on load)
+require("core.file_reload")
 
 -- Make utility functions globally available
 _G.is_windows = require("core.utils").is_windows
