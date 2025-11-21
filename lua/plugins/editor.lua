548f6aeeab33964d84ddbec1eb1729416334b9ac
Yanuo Ma <myn508@hotmail.com>
2025-11-20 23:49:16 -0500

    Update plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -54,7 +54,6 @@ return {
     'esmuellert/vscode-diff.nvim',
     enabled = enabled.vscode_diff,
     pin = false,
-    build = vim.fn.has('win32') == 1 and 'cmd.exe /c build.cmd' or 'bash build.sh',
     cmd = { 'CodeDiff' },
     keys = {
       { '<leader>df', '<cmd>CodeDiff<cr>', desc = 'Code Diff' },
